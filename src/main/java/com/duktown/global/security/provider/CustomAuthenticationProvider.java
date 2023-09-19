748c2f60b7e6181a8b5d9cf7ff171a383a0c8206
sharpie1330 <amberaplin14@gmail.com>
2023-09-20 04:00:59 +0900

    [FEAT #3] AuthenticationProvider 구현

---
+++ src/main/java/com/duktown/global/security/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,43 @@
+package com.duktown.global.security.provider;
+
+import com.duktown.global.security.service.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+// UserDetailsService로 AuthenticationManager로부터 받은 토큰의 username 전달
+// DB에 사용자 존재하는지 확인
+// UserDetails 객체 반환
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    // 입력정보, 실제 사용자 비교
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        CustomUserDetails userDetails = (CustomUserDetails) userDetailsService.loadUserByUsername(email);
+
+        if (userDetails == null || !passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("이메일 또는 비밀번호가 일치하지 않습니다.");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
