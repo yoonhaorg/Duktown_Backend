248d53b4b8dc814727be5cda2a8944dfb01b9c17
sharpie1330 <amberaplin14@gmail.com>
2023-09-20 16:11:41 +0900

    [FEAT #3] AuthenticationSuccessHandler 구현

---
+++ src/main/java/com/duktown/global/security/handler/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,48 @@
+package com.duktown.global.security.handler;
+
+import com.duktown.domain.user.dto.UserDto;
+import com.duktown.domain.user.entity.User;
+import com.duktown.domain.user.entity.UserRepository;
+import com.duktown.global.security.service.CustomUserDetails;
+import com.duktown.global.security.service.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtService;
+
+    @Transactional
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        User user = userRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("가입된 이메일이 존재하지 않습니다."));
+
+        // JWT Token 생성, 응답
+        String accessToken = jwtService.createAccessToken(user.getEmail(), user.getId(), user.getRoleType());
+        String refreshToken = jwtService.createRefreshToken(user.getEmail(), user.getId(), user.getRoleType());
+
+        user.updateRefreshToken(refreshToken);
+        response.setContentType(APPLICATION_JSON_VALUE);
+
+        new ObjectMapper().writeValue(response.getWriter(),
+                new UserDto.UserTokenResponse(user.getRoleType(), accessToken, refreshToken));
+    }
+}
