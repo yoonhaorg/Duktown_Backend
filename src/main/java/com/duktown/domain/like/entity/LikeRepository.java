6eae093743526ed9abe49706fea291ce082b8088
sharpie1330 <amberaplin14@gmail.com>
2023-10-20 03:53:19 +0900

    [STYLE #17] 댓글 삭제 시 좋아요도 함께 삭제 기능 벌크 연산 누락 코드 추가

--- src/main/java/com/duktown/domain/like/entity/LikeRepository.java
+++ src/main/java/com/duktown/domain/like/entity/LikeRepository.java
@@ -1,6 +1,7 @@
 package com.duktown.domain.like.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -24,5 +25,7 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
             "and l.comment.id in :commentIds")
     List<Like> findAllByUserAndCommentIn(@Param("userId") Long userId, @Param("commentIds") List<Long> commentIds);
 
-    void deleteByCommentId(Long commentId);
+    @Modifying
+    @Query("delete from Like l where l.comment.id = :commentId")
+    void deleteByCommentId(@Param("commentId") Long commentId);
 }
