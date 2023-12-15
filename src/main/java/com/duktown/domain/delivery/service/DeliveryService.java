7fc0ee4d8de75ca7297c67a1b871d947a1c4508d
곽태영 <amberaplin14@gmail.com>
2023-12-15 17:58:51 +0900

    [FIX #41] base64 인코딩으로 암복호화 제대로 되지 않던 오류 해결, 암호화 클래스 빈으로 등록

--- src/main/java/com/duktown/domain/delivery/service/DeliveryService.java
+++ src/main/java/com/duktown/domain/delivery/service/DeliveryService.java
@@ -6,14 +6,10 @@ import com.duktown.domain.delivery.entity.DeliveryRepository;
 import com.duktown.domain.user.entity.User;
 import com.duktown.domain.user.entity.UserRepository;
 import com.duktown.global.exception.CustomException;
+import com.duktown.global.kisa_SEED.SEED;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.duktown.global.kisa_SEED.KISA_SEED_CBC;
-
-import java.util.Arrays;
 
 import static com.duktown.global.exception.CustomErrorType.USER_NOT_FOUND;
 
@@ -23,22 +19,14 @@ import static com.duktown.global.exception.CustomErrorType.USER_NOT_FOUND;
 public class DeliveryService {
     private final UserRepository userRepository;
     private final DeliveryRepository deliveryRepository;
-
-    @Value("${custom.seed.pbsz.user.key}")
-    private String secretKey;
-
-    @Value("${custom.seed.pbsz.iv}")
-    private String vectorKey;
+    private final SEED seed;
 
     @Transactional
     public void createDelivery(Long userId, DeliveryDto.CreateRequest request) {
         User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(USER_NOT_FOUND));
-
-        byte[] encryptedAccountNumber = KISA_SEED_CBC.SEED_CBC_Encrypt(
-                secretKey.getBytes(), vectorKey.getBytes(),
-                request.getAccountNumber().getBytes(), 0, request.getAccountNumber().length()
-                );
-        Delivery delivery = request.toEntity(user, Arrays.toString(encryptedAccountNumber));
+        Delivery delivery = request.toEntity(user, seed.encrypt(request.getAccountNumber()));
         deliveryRepository.save(delivery);
     }
+
+
 }
