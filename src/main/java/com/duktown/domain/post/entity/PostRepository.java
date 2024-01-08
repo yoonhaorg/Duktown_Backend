984efacf2a9426661b9c7bcee7776be2be09e4df
sharpie1330 <amberaplin14@gmail.com>
2024-01-08 23:27:47 +0900

    [FEAT #49] 내가 쓴 글, 댓글 단 글 목록 조회 구현

--- src/main/java/com/duktown/domain/post/entity/PostRepository.java
+++ src/main/java/com/duktown/domain/post/entity/PostRepository.java
@@ -9,9 +9,12 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PostRespository extends JpaRepository<Post,Long> {
+public interface PostRepository extends JpaRepository<Post,Long> {
     // 목록 조회 - 페이지네이션, 카테고리
 
     @Query("select p from Post p where p.category = :category order by p.createdAt desc")
     List<Post> findAllByCategory(@Param(value = "category") Category category);
+
+    @Query("select p from Post p where p.user.id = :user_id and p.category = :category order by p.createdAt desc")
+    List<Post> findAllByUserAndCategory(@Param("user_id") Long userId, @Param(value = "category") Category category);
 }
