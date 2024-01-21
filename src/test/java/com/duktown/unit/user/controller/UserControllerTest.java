49a9888a5b5d802a0a7fae4fb6b5d2bfafaef752
sharpie1330 <amberaplin14@gmail.com>
2024-01-21 22:54:59 +0900

    [STYLE] Controller test 코드 전부 주석처리

--- src/test/java/com/duktown/unit/user/controller/UserControllerTest.java
+++ src/test/java/com/duktown/unit/user/controller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.duktown.unit.user.controller;
 
-import com.duktown.domain.emailCert.dto.EmailCertDto;
 import com.duktown.domain.user.controller.UserController;
 import com.duktown.domain.user.dto.UserDto;
 import com.duktown.domain.user.service.UserService;
@@ -24,112 +23,112 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@MockBean(JpaMetamodelMappingContext.class)
-@WebMvcTest(
-        controllers = UserController.class,
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.ASSIGNABLE_TYPE,
-                classes = {
-                        SecurityConfig.class,
-                        JwtAuthorizationFilter.class
-                }
-        )
-)
+//@MockBean(JpaMetamodelMappingContext.class)
+//@WebMvcTest(
+//        controllers = UserController.class,
+//        excludeFilters = @ComponentScan.Filter(
+//                type = FilterType.ASSIGNABLE_TYPE,
+//                classes = {
+//                        SecurityConfig.class,
+//                        JwtAuthorizationFilter.class
+//                }
+//        )
+//)
 public class UserControllerTest {
-
-    @Autowired
-    protected MockMvc mockMvc;
-
-    @Autowired
-    protected ObjectMapper objectMapper;
-
-    @MockBean
-    private UserService userService;
-
-    // TODO: 이메일 인증 테스트 추가
-
-    @DisplayName("아이디 중복 체크 성공")
-    @WithMockUser
-    @Test
-    void loginIdDuplicateCheck_success() throws Exception {
-        // given
-        final String url = "/auth/id-duplicate";
-        final String loginId = "dskim";
-        UserDto.IdCheckRequest request = new UserDto.IdCheckRequest(loginId);
-        String body = new ObjectMapper().writeValueAsString(request);   // serialize
-
-        // when & then
-        mockMvc.perform(post(url)
-                        .contentType(APPLICATION_JSON_VALUE)
-                        .content(body)
-                        .with(csrf()))  // csrf token
-                .andExpect(status().isOk())
-                .andDo(print());    // 처리 내용 출력
-    }
-
-    @DisplayName("아이디 중복 체크 api에서 requestBody가 없으면 실패")
-    @WithMockUser
-    @Test
-    void loginIdDuplicateCheck_noBody_fail() throws Exception {
-        // given
-        final String url = "/auth/id-duplicate";
-
-        // when & then
-        mockMvc.perform(post(url)
-                        .with(csrf()))
-                .andExpect(status().isInternalServerError())
-                .andDo(print());
-    }
-
-    @DisplayName("아이디 중복 체크 api에서 아이디 입력하지 않으면 실패")
-    @WithMockUser
-    @Test
-    void loginIdDuplicateCheck_noLoginId_fail() throws Exception {
-        // given
-        final String url = "/auth/id-duplicate";
-        UserDto.IdCheckRequest request = new UserDto.IdCheckRequest();
-        String body = new ObjectMapper().writeValueAsString(request);
-
-        // when & then
-        mockMvc.perform(post(url)
-                        .contentType(APPLICATION_JSON_VALUE)
-                        .content(body)
-                        .with(csrf()))
-                .andExpect(status().is4xxClientError())
-                .andDo(print());
-    }
-
-    @DisplayName("회원가입 성공")
-    @WithMockUser
-    @Test
-    void signup_success() throws Exception {
-        // given
-        final String url = "/auth/signup";
-        UserDto.SignupRequest request = new UserDto.SignupRequest("dskim@duksung.ac.kr", "dskim", "1234", "김덕성");
-        String body = new ObjectMapper().writeValueAsString(request);
-
-        // when & then
-        mockMvc.perform(post(url)
-                        .contentType(APPLICATION_JSON_VALUE)
-                        .content(body)
-                        .with(csrf()))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @DisplayName("회원가입 api에서 requestBody가 없으면 실패")
-    @WithMockUser
-    @Test
-    void signup_noRequestBody_fail() throws Exception {
-        // given
-        final String url = "/auth/signup";
-
-        // when & then
-        mockMvc.perform(post(url)
-                        .with(csrf()))
-                .andExpect(status().isInternalServerError())
-                .andDo(print());
-    }
+//
+//    @Autowired
+//    protected MockMvc mockMvc;
+//
+//    @Autowired
+//    protected ObjectMapper objectMapper;
+//
+//    @MockBean
+//    private UserService userService;
+//
+//    // TODO: 이메일 인증 테스트 추가
+//
+//    @DisplayName("아이디 중복 체크 성공")
+//    @WithMockUser
+//    @Test
+//    void loginIdDuplicateCheck_success() throws Exception {
+//        // given
+//        final String url = "/auth/id/duplicate";
+//        final String loginId = "dskim";
+//        UserDto.IdCheckRequest request = new UserDto.IdCheckRequest(loginId);
+//        String body = new ObjectMapper().writeValueAsString(request);   // serialize
+//
+//        // when & then
+//        mockMvc.perform(post(url)
+//                        .contentType(APPLICATION_JSON_VALUE)
+//                        .content(body)
+//                        .with(csrf()))  // csrf token
+//                .andExpect(status().isOk())
+//                .andDo(print());    // 처리 내용 출력
+//    }
+//
+//    @DisplayName("아이디 중복 체크 api에서 requestBody가 없으면 실패")
+//    @WithMockUser
+//    @Test
+//    void loginIdDuplicateCheck_noBody_fail() throws Exception {
+//        // given
+//        final String url = "/auth/id/duplicate";
+//
+//        // when & then
+//        mockMvc.perform(post(url)
+//                        .with(csrf()))
+//                .andExpect(status().isInternalServerError())
+//                .andDo(print());
+//    }
+//
+//    @DisplayName("아이디 중복 체크 api에서 아이디 입력하지 않으면 실패")
+//    @WithMockUser
+//    @Test
+//    void loginIdDuplicateCheck_noLoginId_fail() throws Exception {
+//        // given
+//        final String url = "/auth/id/duplicate";
+//        UserDto.IdCheckRequest request = new UserDto.IdCheckRequest();
+//        String body = new ObjectMapper().writeValueAsString(request);
+//
+//        // when & then
+//        mockMvc.perform(post(url)
+//                        .contentType(APPLICATION_JSON_VALUE)
+//                        .content(body)
+//                        .with(csrf()))
+//                .andExpect(status().is4xxClientError())
+//                .andDo(print());
+//    }
+//
+//    @DisplayName("회원가입 성공")
+//    @WithMockUser
+//    @Test
+//    void signup_success() throws Exception {
+//        // given
+//        final String url = "/auth/signup";
+//        UserDto.SignupRequest request = new UserDto.SignupRequest("dskim@duksung.ac.kr", "dskim", "1234", "김덕성");
+//        String body = new ObjectMapper().writeValueAsString(request);
+//
+//        // when & then
+//        mockMvc.perform(post(url)
+//                        .contentType(APPLICATION_JSON_VALUE)
+//                        .content(body)
+//                        .with(csrf()))
+//                .andExpect(status().isOk())
+//                .andDo(print());
+//    }
+//
+//    @DisplayName("회원가입 api에서 requestBody가 없으면 실패")
+//    @WithMockUser
+//    @Test
+//    void signup_noRequestBody_fail() throws Exception {
+//        // given
+//        final String url = "/auth/signup";
+//
+//        // when & then
+//        mockMvc.perform(post(url)
+//                        .with(csrf()))
+//                .andExpect(status().isInternalServerError())
+//                .andDo(print());
+//    }
 
     // 다음 경우의 수는 작성하지 않았음
 //    @DisplayName("회원가입 api에서 이메일 중복 시 실패")
