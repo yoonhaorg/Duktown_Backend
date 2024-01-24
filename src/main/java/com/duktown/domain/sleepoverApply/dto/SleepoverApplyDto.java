7aae0e0554365dd23f5c6548a780ac55d55dfc71
hyesoo kim <khwater1017@naver.com>
2024-01-24 21:21:24 +0900

    [FIX #46]상세 주소 누락

--- src/main/java/com/duktown/domain/sleepoverApply/dto/SleepoverApplyDto.java
+++ src/main/java/com/duktown/domain/sleepoverApply/dto/SleepoverApplyDto.java
@@ -67,7 +67,7 @@ public class SleepoverApplyDto {
             startDate = sleepoverApply.getStartDate();
             endDate= sleepoverApply.getEndDate();
             period= sleepoverApply.getPeriod();
-            address=  sleepoverApply.getAddress().getStreetAddress() + sleepoverApply.getAddress().getStreetAddress();
+            address=  sleepoverApply.getAddress().getStreetAddress() + " "+sleepoverApply.getAddress().getDetailAddress();
             reason =sleepoverApply.getReason();
             userId = sleepoverApply.getUser().getId();
 
